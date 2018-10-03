7c02846f085c6711b82ccc0612b905db8642530d
Jefferson Lam <jeffersonwlam@gmail.com>
2018-10-02 23:39:20 -0500

    Remove babel
    It keeps breaking, preventing JS from being piped into dist

--- gulpfile.js
+++ gulpfile.js
@@ -31,11 +31,8 @@ gulp.task('html:watch', function() {
 
 gulp.task('js', function() {
   gulp.src('./src/js/*.js')
-    .pipe(babel({
-      presets: ['@babel/preset-env']
-    }))
-    .pipe(gulp.dest('dist'))
-    .pipe(reload({stream:true}));
+    .pipe(reload({stream:true}))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('js:watch', function() {
